d20f7cc3cd49b7d0429e9d09c576b1db90f2b18e
Jerson Kyle Bonifacio <143814993+JisuKayl@users.noreply.github.com>
2024-09-22 20:58:13 +0800

    feat: added the initial implementaiton of websocket for realtime two way communication

--- server.js
+++ server.js
@@ -1,21 +1,41 @@
 const express = require("express");
+const http = require("http");
+const { WebSocketServer } = require("ws");
+
 const bodyParser = require("body-parser");
-const app = express();
-const routes = require("./src/routes/index");
 const cookieParser = require("cookie-parser");
+const routes = require("./src/routes/index");
 require("./src/database/mongoose");
 require("dotenv").config();
 const port = process.env.PORT;
+
+const app = express();
+const server = http.createServer(app);
+
+const wss = new WebSocketServer({ server: server });
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(routes);
 
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+
+  ws.on("message", (message) => {
+    console.log(`Sent: ${message}`);
+    ws.send(`Received: ${message}`);
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
 app.get("/", (req, res) => {
   res.send("Case Home");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is running in port http://localhost:${port}`);
 });
 
